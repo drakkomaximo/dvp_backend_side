ab0343d5436b0fa88be4c4ab57353839bd446e56
drakkomaximo <drakkomaximo1234@gmail.com>
2023-07-24 18:25:39 -0500

    update controllers

--- db/database.v2.sql
+++ db/database.v2.sql
@@ -14,15 +14,20 @@ VALUES ('Nombre1'),
 
 SELECT * FROM accounts;
 
-CREATE TABLE selected_list (
+CREATE TABLE github_users_list (
     id INT PRIMARY KEY AUTO_INCREMENT,
     account_id INT,
-    github_user_name VARCHAR(100),
+    user_name VARCHAR(100),
+    user_id INT,
+    user_avatar VARCHAR(100),
+    user_github_link VARCHAR(100),
     FOREIGN KEY (account_id) REFERENCES accounts(id)
 );
 
-INSERT INTO selectedlist (account_id, github_user_name)
-VALUES (1, 'drakkomaximo');
+INSERT INTO github_users_list (account_id, user_name, user_id, user_avatar, user_github_link)
+VALUES (1, 'drakkomaximo', 57687342, 'https://avatars.githubusercontent.com/u/57687342?v=4', 'https://github.com/drakkomaximo');
 
-SELECT * FROM selectedlist;
-SELECT * FROM selectedlist WHERE account_id = 1;
+SELECT * FROM github_users_list;
+SELECT * FROM github_users_list WHERE account_id = 1;
+
+DELETE FROM github_users_list where user_id=57687342;
--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -5,17 +5,19 @@ export const searchUsersByName = async (req, res) => {
   const { userName } = req.params;
 
   try {
-    const response = await axiosInstance.get(`/search/users?q=${userName}&page=1&per_page=10`);
-    res.status(200).json({ 
-        status: 200,
-        data: {...response.data }
-     });
+    const response = await axiosInstance.get(
+      `/search/users?q=${userName}&page=1&per_page=10`
+    );
+    res.status(200).json({
+      status: 200,
+      data: { ...response.data },
+    });
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({ 
-        status: 500,
-        data: 'Error en el servidor'
-     });
+    console.log(error);
+    return res.status(500).json({
+      status: 500,
+      data: "Error en el servidor",
+    });
   }
 };
 
@@ -23,63 +25,72 @@ export const getUserByName = async (req, res) => {
   const { userName } = req.params;
   try {
     const response = await axiosInstance.get(`/users/${userName}`);
-    res.status(200).json({ 
-        status: 200,
-        data: {...response.data}
-     });
+    res.status(200).json({
+      status: 200,
+      data: { ...response.data },
+    });
   } catch (error) {
-    return res.status(500).json({ 
-        status: 500,
-        data: 'Error en el servidor'
-     });
+    console.log(error);
+    return res.status(500).json({
+      status: 500,
+      data: "Error en el servidor",
+    });
   }
 };
 
 export const selectUserByName = async (req, res) => {
-  const { selectedName, id } = req.body;
+  const { user, id } = req.body;
+  const { avatar, userId, username, githubLink } = user;
   try {
-
     const dbResponse = await pool.query("SELECT * FROM accounts WHERE id = ?", [
       id,
-    ])
+    ]);
 
-    if (dbResponse[0][0]?.id){
+    if (dbResponse[0][0]?.id) {
+      const result = await pool.query(
+        "SELECT * FROM github_users_list WHERE user_name = ?",
+        [username]
+      );
 
-      const result = await pool.query("SELECT * FROM selected_list WHERE github_user_name = ?", [
-        selectedName,
-      ])
-
-      if(result[0].length > 0){
+      if (result[0].length > 0) {
         return res.status(200).json({
           status: 200,
           data: `El usuario seleccionado ya se encuentra en la lista`,
         });
       }
 
-      await pool.query("INSERT INTO selected_list SET ?", {
+      await pool.query("INSERT INTO github_users_list SET ?", {
         account_id: dbResponse[0][0]?.id,
-        github_user_name: selectedName,
+        user_name: username,
+        user_id: userId,
+        user_avatar: avatar,
+        user_github_link: githubLink,
       });
 
       res.json({
         status: 200,
-        data: 'Selection has been saved',
+        data: "Selection has been saved",
+        account_id: dbResponse[0][0]?.id,
+        user_name: username
       });
-    }else{
+    } else {
       const result = await pool.query("INSERT INTO accounts SET ?", {
         account_name: `chamo${id}`,
       });
 
-      if(result[0].insertId){
-        await pool.query("INSERT INTO selected_list SET ?", {
-          account_id: result[0].insertId,
-          github_user_name: selectedName,
-        });
-      }
+      await pool.query("INSERT INTO github_users_list SET ?", {
+        account_id: result[0].insertId,
+        user_name: username,
+        user_id: userId,
+        user_avatar: avatar,
+        user_github_link: githubLink,
+      });
 
       res.json({
         status: 200,
-        data: 'Selection has been saved',
+        data: "Selection has been saved",
+        account_id: result[0].insertId,
+        user_name: username
       });
     }
   } catch (error) {
@@ -93,17 +104,24 @@ export const selectUserByName = async (req, res) => {
 export const getSelectedUsersById = async (req, res) => {
   const { id } = req.params;
   try {
+    const dbResponse = await pool.query(
+      "SELECT * FROM github_users_list WHERE account_id = ?",
+      [id]
+    );
 
-    const dbResponse = await pool.query("SELECT * FROM selected_list WHERE account_id = ?", [
-      id,
-    ])
-
-    if(dbResponse[0].length > 0){
+    console.log(dbResponse[0]);
+    if (dbResponse[0].length > 0) {
+      const formatedResponse = dbResponse[0].map((user) => ({
+        user_avatar: user.user_avatar,
+        user_id: user.user_id,
+        user_name: user.user_name,
+        user_github_link: user.user_github_link,
+      }));
       res.json({
         status: 200,
-        data: dbResponse[0],
+        data: formatedResponse,
       });
-    }else{
+    } else {
       res.json({
         status: 200,
         data: [],
